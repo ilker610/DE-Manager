ba66857cd8a9d987a2ef8dcb924cf3cad514b2f5
İlker Sezer <75069363+ilker610@users.noreply.github.com>
2022-06-28 00:16:28 +0300

    Add files via upload

--- modules/install_de.py
+++ modules/install_de.py
@@ -34,3 +34,32 @@ def install(de = None):
             messagebox.showinfo('Succes',f"{de} succesfully installed.")
         else:
             messagebox.showerror("Permissions Error", f'[-DE Manager-] I cant found "{de}" in my database :(')
+
+def uninstall(de = None):
+    if de == None:
+        messagebox.showerror('Permissions Error', 'You must be select desktop environment >:(')
+    elif not os.environ.get("SUDO_UID") and os.geteuid() != 0:
+        messagebox.showerror('Permissions Error', 'You must be root >:(')
+        exit()
+    else:
+        if de == "Xfce":
+            de = "xfce4 xfce4-goodies"
+        elif de == "Gnome":
+            de = "gnome"
+        elif de == "KDE Plasma":
+            de = "plasma kde-applications"
+        elif de == "LXQT":
+            de = "lxqt"
+        elif de == "LXDE":
+            de = "lxde"
+        elif de == "Cinnamon":
+            de = "cinnamon"
+        elif de == "Budgie":
+            de = "budgie-desktop"
+        elif de == "Mate":
+            de = "mate mate-extra"
+        else:
+            pass
+
+        os.system(f"pacman -Rscn {de} --noconfirm")
+        messagebox.showinfo('Success',f"{de} packages uninstalled.")
